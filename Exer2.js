08d92bc47f80f1f22ea7e66f9a59d39579ddaad8
sheilaschlickmann <61628827+sheilaschlickmann@users.noreply.github.com>
2020-05-20 19:22:40 -0300

    Update Exer2.js

--- Exer2.js
+++ Exer2.js
@@ -1,6 +1,14 @@
 var listElement = document.querySelector('ul');
 var inputElement = document.querySelector('input');
 
+function mostraLoading(loading) {
+  listElement.innerHTML = "";
+  var texto = document.createTextNode('Carregando...');
+  var loadingElement = document.createElement('li');
+  loadingElement.appendChild(texto);
+  listElement.appendChild(loadingElement);
+}
+
 function preencheLista(lista){
     for(list of lista){
         const textElement = document.createTextNode(list.name);
@@ -13,10 +21,11 @@ function preencheLista(lista){
 
 function buscarUser(){
     var user = inputElement.value;
+    mostraLoading();
 
     axios.get('https://api.github.com/users/' + user + '/repos')
     .then(function (response) {
-        preencheLista(response.data);
+        console.log(preencheLista(response.data));
     })
     .catch(function(error){
         console.warn(error);
